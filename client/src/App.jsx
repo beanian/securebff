3914201b5ccac01c647565785d4a5c3b3433ac6d
Ian Bennett <ianbennett24@gmail.com>
2025-04-24 12:32:46 +0100

    fix backtick issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const API_BASE = import.meta.env.VITE_API_BASE || "";
 
   useEffect(() => {
-    fetch("${API_BASE}/csrf-token", {
+    fetch(`${API_BASE}/csrf-token`, {
       credentials: "include"
     })
       .then((res) => res.json())
@@ -15,12 +15,12 @@ function App() {
   }, []);
 
   const handleCheck = async () => {
-    const csrfRes = await fetch("${API_BASE}/csrf-token", {
+    const csrfRes = await fetch(`${API_BASE}/csrf-token`, {
       credentials: "include"
     });
     const { csrf: freshToken } = await csrfRes.json();
   
-    const res = await fetch("${API_BASE}/check-account", {
+    const res = await fetch(`${API_BASE}/check-account`, {
       method: "POST",
       credentials: "include",
       headers: {
